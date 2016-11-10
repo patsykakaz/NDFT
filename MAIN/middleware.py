7ae0c66705eee522c521f95dafa6482e95a8676c
patsykakaz <philippe@lesidecar.fr>
2016-11-10 17:06:58 +0100

    middleware

--- MAIN/middleware.py
+++ MAIN/middleware.py
@@ -12,10 +12,12 @@ from mezzanine.conf import settings
 from models import *
 
 class MenuMiddleware(object):
-    def process_request(self,request):
+    def process_template_response(self, request, response):
         all_categories = Category.objects.all()
         # forbidden_domain = "lalettre"
         # if not request.user.is_authenticated() and not "admin/" in request.path and not "/user/" in request.path :
         #     return HttpResponseRedirect('/user/login/?next='+request.path)
         # else:
         #     print "request.user.is_authenticated = {}".format(request.user.is_authenticated())
+        response.context_data['all_categories'] = all_categories
+        return response
